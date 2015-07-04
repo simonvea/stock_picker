2b98b9e5ab10f770d3774accf74580c6e5a1e6ce
Simon Opheim <simon.opheim@gmail.com>
2015-07-04 20:08:52 +0200

    created basic method

---
+++ stock_picker.rb
@@ -0,0 +1,15 @@
+#kjøp lav først, selg høg
+
+def stock_picker(prices = [0])
+    
+    topp = 0
+    bunn = 10000000
+    profitt = 0
+    
+    prices.each_with_index do |price, index|
+        
+    end
+        
+        
+
+end
